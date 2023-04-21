9e4d898eda067b39df6c02b4572f9bde8e0bb2ca
bRishiSai <brishisai@teksystems.com>
2023-04-21 13:34:45 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,40 @@
+import logo from './logo.svg';
+import './App.css';
+import NavBar from './component/layout/Navbar';
+import Users from './component/pokemons/Users';
+import { Component } from 'react';
+import axios from 'axios';
+import Search from './component/pokemons/search';
+import Page from './component/pokemons/Page';
+
+class App extends Component {
+  state={
+    pokemons:[]
+  }
+  async componentDidMount()
+  {
+    const res =await axios.get(`http://localhost:5000/users`);
+    this.setState({pokemons:res.data})
+  }
+
+  searchPokemons= async (text) => {
+    console.log(text)
+    const res =await axios.get(`http://localhost:5000/users?q=${text}`);
+    this.setState({pokemons:res.data})
+  }
+
+  render(){
+  return (
+    <div className="App">
+      <NavBar title="Pokemon Search Application"/>
+      <Search searchPokemons={this.searchPokemons}/>
+      
+      <div>
+        <Page pokemons={this.state.pokemons}/>
+      </div>
+</div>
+  );
+}
+}
+
+export default App;
